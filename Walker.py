9921f595a8c6a3838d251062e41b4612a1602605
BiggiePete <peter.cross222@gmail.com>
2022-04-06 17:40:28 -0400

    Hard Coded Walk Cycle - Started

--- Walker.py
+++ Walker.py
@@ -1,4 +1,6 @@
+from time import sleep
 from Servo import Servo
+from Walk import _walk
 
 
 class Walker:
@@ -30,3 +32,39 @@ class Walker:
 
     def Squat(self):
         pass
+    
+    def LiftLeg(self, r):
+        """Lifts a leg
+
+        Args:
+            r (Int): Anything > 0 is the right leg
+
+        Returns:
+            Int: Number of seconds estimated that it will take for the rotation to finish, it is smart to run these statements inside of a sleep command
+        """
+        if r > 0:
+            #lift right leg
+            self.RTHigh.SetAngle(200)
+            sleep(0.2)
+            self.RShin.SetAngle(270 - self.RTHigh.GetAngle() + 20)
+            return 2
+        else:
+            #lift left leg
+            self.LTHigh.SetAngle(200)
+            sleep(0.2)
+            self.LShin.SetAngle(270 - self.LThigh.GetAngle() + 20)
+            return 2
+
+    def step(self,r):
+        if r > 0:
+            #step the right leg
+            self.RTHigh.SetAngle(150)
+            sleep(0.2)
+            self.RShin.SetAngle(270 - self.RTHigh.GetAngle())
+            return 2
+        else:
+            #step the left leg
+            self.LThigh.SetAngle(150)
+            sleep(0.2)
+            self.LShin.SetAngle(270 - self.LThigh.GetAngle())
+            return 2
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from Walker import Walker as w
 from adafruit_servokit import ServoKit as sk
 import numpy as np
@@ -7,5 +8,39 @@ def init():
     print("Setting all Servos to their resting State")
     kit = sk(channels=16)
     biped = w(kit)
+
+    biped.Reset()
+    while True:
+        print("w = walk")
+        print("s = squat")
+        print("r = reset")
+        x = input("Command : ")
+        if x == "w":
+            run(2)
+        elif x == "s":
+            biped.Squat()
+        elif x == "r":
+            biped.Reset()
+        
+
+def run(s):
+    print("Taking " + str(s) + " Steps!")
+    # first order of buissiness is to find out how many times we need to move each leg
+    l_leg_steps = np.mod(s,2)
+    r_leg_steps = l_leg_steps + 1
+
+
+    # we wil always step with our right leg first
+    
+    #lets first detect which leg to step with, inside our loop
+    while s > 0:
+        s-=1
+        if r_leg_steps > l_leg_steps:
+            #right leg step
+            sleep(w.LiftLeg(1))
+        else:
+            #left leg step
+            sleep(w.LiftLeg(0))
+        
     
 init()
